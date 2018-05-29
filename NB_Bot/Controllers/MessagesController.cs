03908e1cdeb4e40905964c5e4dbb8a83aa129654
Si Thu Win <sthuwin@gmail.com>
2018-05-30 00:03:44 +0800

    added Luis

--- NB_Bot/Controllers/MessagesController.cs
+++ NB_Bot/Controllers/MessagesController.cs
@@ -1,10 +1,12 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using NB_Bot.Dialogs;
+using NB_Bot.Models;
 
 namespace NB_Bot
 {
@@ -19,7 +21,7 @@ namespace NB_Bot
         {
             if (activity.GetActivityType() == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => HotelBotDialog.dialog);
+                await Conversation.SendAsync(activity, MakeLuisDIalog);
             }
             else
             {
@@ -27,6 +29,11 @@ namespace NB_Bot
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
+        }
+
+        private IDialog<RoomReservation> MakeLuisDIalog()
+        {
+            return Chain.From(() => new LuisDialog(RoomReservation.BuildForm));
         }
 
         private Activity HandleSystemMessage(Activity message)
